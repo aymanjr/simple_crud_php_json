16695928b7e68755f84cd2773c61a46409fd387c
ayman jrm <ayman.jarmoune@outlook.fr>
2021-12-15 01:01:14 +0100

    creating index and etudiant file

---
+++ etudiant.php
@@ -0,0 +1,28 @@
+<?php
+
+
+function getUsers()
+{
+     $etudiant =   json_decode(file_get_contents(filename: __DIR__ . 'etudiant.json'));
+}
+
+
+function getUserbyid($id)
+{
+}
+
+
+
+function createUser($data)
+{
+}
+
+
+function updateUser($data, $id)
+{
+}
+
+
+function deleteUser($id)
+{
+}
