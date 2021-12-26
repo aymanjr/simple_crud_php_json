d0791e46d9f67b2a3232b79720151c24f848f54b
ayman jrm <ayman.jarmoune@outlook.fr>
2021-12-26 09:37:16 +0100

    changing the index matiers

--- MATIERS/index.php
+++ MATIERS/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    include './configdata.php'
+include './configdata.php'
 
 ?>
 <!doctype html>
@@ -25,85 +25,102 @@
 <body class="" style=" background: linear-gradient(
 35deg, #66fcf1, #45a29e);
    ">
-    
 
-    <div class="container">
-        <?php
-          $result = mysqli_query($conn, "SELECT * FROM `matier`  ");
-           echo"<center>";
-           echo"<hr/>";
-           echo"<select id='rech'>";
-           echo"<option>-- Recherche Matier par Nom</option>";
-           while($row  = mysqli_fetch_array($result)){
+    <form name="form" action="" method="post">
+        <div class="container">
+            <?php
+            $result = mysqli_query($conn, "SELECT * FROM `matier`  ");
+            echo "<center>";
+            echo "<hr/>";
+            echo "<select id='rech'>";
+            echo "<option>-- Recherche Matier par Nom</option>";
+            while ($row  = mysqli_fetch_array($result)) {
 
-            echo"<option>$row[nom_matier]</option>";
-           }
-           echo"</select>";
-          echo"</center>";
-         
-
-
-        ?>
-
-        <div class="row">
-            <div class="col-lg-10 offset mt-3">
-                <table class="table table-striped table-bordered bg-light">
-                    <thead>
-                        <tr class="bg-dark text-light text-center">
-                            <th>ID</th>
-                            <th>NOM & PRENOM</th>
-                            <th>Note :</th>
+                echo "<option value='$row[id_matier]' >$row[nom_matier]</option>";
+            }
+            echo "</select>";
+            echo "</center>";
 
 
 
-                            <th>EDIT</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $sql = "SELECT * FROM etudiant";
-                        $result = mysqli_query($conn, $sql);
+            ?>
 
-                        if (mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                      
-                        ?>
-
-                                <tr class="text-center">
-                                    <td><?= $row['id']; ?></td>
-                                    <td><?= $row['nom']; ?></td>
-                                    <td><input type="number" for="note"></td>
-                            
+            <div class="row">
+                <div class="col-lg-10 offset mt-3">
+                    <table class="table table-striped table-bordered bg-light">
+                        <thead>
+                            <tr class="bg-dark text-light text-center">
+                                <th>ID</th>
+                                <th>NOM & PRENOM</th>
+                                <th>Note :</th>
 
 
-                                    <td><a class="btn btn-info" href="note.php?id=<?= $row['id'];?>,nom=<?=$row['nom'];?>">ADD</a></td>
-                                </tr>
-                        <?php }
-                        } ?>
-                    </tbody>
-                </table>
+
+                                <th>EDIT</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "SELECT * FROM etudiant";
+                            $result = mysqli_query($conn, $sql);
+
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+
+                            ?>
+                                
+                                    <tr class="text-center">
+                                        <td><?= $row['id']; ?> <input type="hidden" id="id_etd" value="<?= $row['id']; ?>"></td>
+                                        <td><?= $row['nom']; ?></td>
+                                        <td><input type="number" for="note" name="nt" id="note_a"></td>
 
 
-                <div class="form-group text-center">
-                    <!-- <a class="btn btn-warning text-center" href="showdata.php">Ajouter nouvel Etudiant</a> -->
+                                        <td><a class="btn btn-info" href="note.php?id=<?= $row['id']; ?>">ADD</a></td>
+
+                                        <?
+
+                                        $id_etd = $row['id'];
+                                        $lanote =  $_POST['nt'];
+
+                                        
+                                        ?>
+
+
+                                    </tr>
+                            <?php }
+                            } ?>
+                        </tbody>
+                    </table>
+        
+
+                    <div class="form-group text-center">
+                        <!-- <a class="btn btn-warning text-center" href="showdata.php">Ajouter nouvel Etudiant</a> -->
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js" integrity="sha512-rMGGF4wg1R73ehtnxXBt5mbUfN9JUJwbk21KMlnLZDJh7BkPmeovBuddZCENJddHYYMkCh9hPFnPmS9sspki8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" integrity="sha512-yVvxUQV0QESBt1SyZbNJMAwyKvFTLMyXSyBHDO4BG5t7k/Lw34tyqlSDlKIrIENIzCl+RVUNjmCPG+V/GMesRw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js" integrity="sha512-rMGGF4wg1R73ehtnxXBt5mbUfN9JUJwbk21KMlnLZDJh7BkPmeovBuddZCENJddHYYMkCh9hPFnPmS9sspki8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" integrity="sha512-yVvxUQV0QESBt1SyZbNJMAwyKvFTLMyXSyBHDO4BG5t7k/Lw34tyqlSDlKIrIENIzCl+RVUNjmCPG+V/GMesRw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <script>
-        $("#rech").chosen();
-    </script>
+        <script>
+            $("#rech").chosen();
+              
+            var id_mat = document.getElementById('rech').value;
+            var id_etd = document.getElementById('id_etd').value;
+            var note_a = document.getElementById('note_a').value;
 
+
+
+        </script>
+    </form>
+       
 </body>
 
 </html>
--- MATIERS/note.php
+++ MATIERS/note.php
@@ -14,9 +14,4 @@
         echo "Error";
     } 
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-      } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-      }
 ?>
